b13e2ad48528b4ea55e699b542c03bb6f7037449
Luciano Martins <luciano.martins@quay.com.br>
2025-02-21 09:58:38 -0300

    acesso a rota urls_public

--- kernel/settings.py
+++ kernel/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
 LOGIN_URL = 'login'  # URL para redirecionamento quando @login_required falhar
 
 ROOT_URLCONF = 'kernel.urls'
-# PUBLIC_SCHEMA_URLCONF = 'kernel.urls_public'
+PUBLIC_SCHEMA_URLCONF = 'kernel.urls_public'
 
 TEMPLATES = [
     {
